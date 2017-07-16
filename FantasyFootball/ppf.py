05ee02025a4fac51592f64c031e04e22f07f6ca1
Angela Stanchie <astanchi@advent.com>
2017-07-15 18:28:59 -0700

    Add python script to scrape Pro Fantasy Football player rank

--- FantasyFootball/ppf.py
+++ FantasyFootball/ppf.py
@@ -1,24 +1,51 @@
-#https://www.profootballfocus.com/nfl/teams/buffalo-bills/4/roster
-from lxml import html
-from lxml import etree
 import requests
+import os
+import csv
+from bs4 import BeautifulSoup
 
-page = requests.get('https://www.profootballfocus.com/nfl/teams/buffalo-bills/4/roster')
-tree = html.fromstring(page.content)
-#print(etree.tostring(tree, pretty_print=True))
+players_list = []
+ppf_url = 'https://www.profootballfocus.com'
 
+teams_url = ppf_url + '/nfl/teams'
+teams_page = requests.get(teams_url)
+teams_soup = BeautifulSoup(teams_page.text, 'lxml')
 
-#This will create a list of buyers:
-position = tree.xpath('//tr[@class="roster-header"]//td/text()')
-#This will create a list of prices
-#prices = tree.xpath('//span[@class="item-price"]/text()')
-players = tree.xpath('//tr//td//a/text()')
+teams = teams_soup.find_all('a', href=True)
+for team in teams:
+	team_url = team['href']
+	if ('/nfl/teams/' in team_url):
+		team_url = ppf_url + team_url + "/roster"
+		#print(team_url)
+		team_page = requests.get(team_url)
+		team_soup = BeautifulSoup(team_page.text,'lxml')
+		players = team_soup.find_all('a', href=True)
+		for player in players:
+			player_url = player['href']
+			if ('/nfl/players' in player_url):
+				#print(player_url)
+				player_url = ppf_url + player_url
+				player_page = requests.get(player_url)
+				player_soup = BeautifulSoup(player_page.text,'lxml')
+				
+				player_title = player_soup.title.string.split(" | ")
+				temp = player_title[1]
+				temp_sp_idx = temp.rfind(' ')
+				team = temp[:temp_sp_idx]
+				position = temp[temp_sp_idx+1:]
+				name = player_soup.h1.find_all('span')[-1].string
+				grades = player_soup.body.findAll(text='2016 PFF Grades')
+				if len(grades) > 0:
+					rank = float(player_soup.table.tr.find_all('td')[1].string)
+				else:
+					rank = 99999999999999
 
-print ('Positions: ', position)
-print ('Players: ', players)
+				#print(f"team: {team} position: {position} player name: {name} rank: {rank}")
+				player_list =[team, position, name, rank]
+				players_list.append(player_list)
 
-# elems = tree.xpath('//table[@class="table-draftmaster table-roster"]//tbody//tr/text()')
-# print('Elements: ', elems)
-
-for e in tree.xpath('//table[@class="table table-draftmaster table-roster table-responsive ui-table"]//tbody//tr//td/a'):
-	print(e.text)
+root_path = os.getcwd()
+out_file = os.path.join(root_path, 'ppf.csv')
+with open(out_file,"w", newline="") as fh:
+	csvwriter = csv.writer(fh,delimiter=",")
+	csvwriter.writerow(["team","position","name","rank"])
+	csvwriter.writerows(players_list)
